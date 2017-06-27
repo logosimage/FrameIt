8ab9216c2d4e89ae3aaf7d72578eea29a82ed30c
Larry Davis <davispcsolutions@gmail.com>
2017-06-27 11:57:01 -0700

    added tag feature

--- pictureThis/mypictures/admin.py
+++ pictureThis/mypictures/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Image, Tag
 
-admin.site.register(Blog)
-# admin.site.register(Image)
-# admin.site.register(Tag)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_tags')
+    # fields = ['title', 'content', ('author','tags'),'image']
+    list_filter = ('tags',)
+    fieldsets = (
+        ('None', {
+            'fields':('title', 'content')
+        }),
+        ('other Stuff', {
+            'fields':('author','image', "tags")
+        }),
+    )
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Image)
+admin.site.register(Tag)
 
 
--- pictureThis/mypictures/models.py
+++ pictureThis/mypictures/models.py
@@ -4,9 +4,8 @@ from django.utils.text import slugify
 import datetime
 
 
-def upload_handler(instance,filename):
-    return "{date}/{file}".format(date=datetime.date.today(),file=filename)
-
+def upload_handler(instance, filename):
+    return "{date}/{file}".format(date=datetime.date.today(), file=filename)
 
 
 class Blog(models.Model):
@@ -15,43 +14,39 @@ class Blog(models.Model):
     author = models.ForeignKey(User, blank=True, null=True)
     image = models.ForeignKey('Image', blank=True, null=True)
     slug = models.SlugField(null=True, blank=True)
-    tags = models.ManyToManyField('Tags', blank=True, null=True)
-
-
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
 
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs ):
+    def display_tags(self):
+        return ', '.join([tag.name for tag in self.tags.all()])
+
+    def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
 
         super(Blog, self).save(*args, **kwargs)
 
 
-        class Image(models.Model):
-            name = models.CharField(max_length=255)
-            img = models.ImageField(upload_to=upload_handler)
-            alt = models.CharField(max_length=255, blank=True, null=True)
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to=upload_handler)
+    alt = models.CharField(max_length=255, blank=True, null=True)
 
-            def __str__(self):
-                return self.name
-
-
+    def __str__(self):
+        return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
 
-        class Tag(models.Model):
-            name = models.CharField(max_length=255)
-            desc = models.TextField(blank=True, null=True)
-            slug = models.SlugField(blank=True, null=True)
-            tags = models.ManyToManyField('Tags', blank=True)
-
-            def __str__(self):
-                return self.name
-
-            def save(self, *args, **kwargs):
-                if not self.slug:
-                    self.slug = slugify(self.name)
-                super(Tag, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
